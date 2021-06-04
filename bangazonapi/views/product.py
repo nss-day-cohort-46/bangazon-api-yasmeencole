8a338673e4df00c028029dd65824fd55b94dbfb9
yasmeencole <yasmeencole1@mail.com>
2021-06-04 09:13:54 -0500

    fixed error on line 257 now able to get product by location

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -254,7 +254,7 @@ class Products(ViewSet):
         direction = self.request.query_params.get('direction', None)
         number_sold = self.request.query_params.get('number_sold', None)
         min_price = self.request.query_params.get('min_price', None)
-        # location = self.reqest.query_params.get('location', None)
+        location = self.request.query_params.get('location', None)
 
         if order is not None:
             order_filter = order
@@ -282,8 +282,8 @@ class Products(ViewSet):
         if min_price is not None:
             products = products.filter(price__gte = min_price)    
             
-        # if location is not None:
-        #     products = products.filter(location__contains = location)    
+        if location is not None:
+            products = products.filter(location__contains = location)    
 
         serializer = ProductSerializer(
             products, many=True, context={'request': request})
